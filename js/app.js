29f4edd17f3caca11ab37450d44bc85654165047
Arvin Singla <arvin.singla@gmail.com>
2014-05-29 12:56:59 -0400

    Almost completed converting styleguide to markup/css

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+(function ($) {
+
+  // Ensure textfields in a group properly overlap.
+  $('.text-group input').each(function(index) {
+    if (index) {
+      $(this).css('top', '-' + index + 'px');
+    }
+  });
+
+  // Ensure textfields in a group properly overlap.
+  $('.radio-group label').each(function(index) {
+    if (index) {
+      $(this).css('left', '-' + index + 'px');
+    }
+  });
+
+  // Ensure radio group border changes when highlighted
+  $('.radio-group input').change(function(e) {
+    $(this).parents('.radio-group').addClass('selected');
+  })
+
+}(jQuery));
