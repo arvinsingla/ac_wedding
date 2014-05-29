4964c624abbd7b52e151c03318b92aecffeb64a7
Arvin Singla <arvin.singla@gmail.com>
2014-05-29 15:48:53 -0400

    Fixes for vQC issues.

--- js/app.js
+++ js/app.js
@@ -4,14 +4,14 @@ $(document).foundation();
 
 (function ($) {
 
-  // Ensure textfields in a group properly overlap.
+  // Ensure textfields in a group properly overlap vertically.
   $('.text-group input').each(function(index) {
     if (index) {
       $(this).css('top', '-' + index + 'px');
     }
   });
 
-  // Ensure textfields in a group properly overlap.
+  // Ensure textfields in a group properly overlap horizontally.
   $('.radio-group label').each(function(index) {
     if (index) {
       $(this).css('left', '-' + index + 'px');
@@ -22,5 +22,15 @@ $(document).foundation();
   $('.radio-group input').change(function(e) {
     $(this).parents('.radio-group').addClass('selected');
   })
+
+  // Select list: Add class when selected
+  $('select').change(function(e) {
+    if ($(this).val() !== "") {
+      $(this).addClass('selected');
+    }
+    else {
+      $(this).removeClass('selected');
+    }
+  });
 
 }(jQuery));
