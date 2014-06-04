d5506e855f9b9676e26828ef61e784d88a1cac1f
Arvin Singla <arvin.singla@gmail.com>
2014-06-04 19:36:37 -0400

    New organizational structure for Sass, Markup for site foundation.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,27 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
+// jQuery specific fixes for the front end.
 (function ($) {
+
+  // Enable lazy loading for images.
+  /*
+  $('img.bttrlazyloading').bttrlazyloading({
+    animation: 'fadeIn',
+    backgroundcolor: 'transparent'
+  });
+  */
+  var s = skrollr.init({
+    forceHeight: true,
+    render: function(data) {
+      //Debugging - Log the current scroll position.
+    }
+  });
+
+  // Ensure each row has a higher z-index then the one below it.
+  $('div.row').each(function(index) {
+    $(this).css('z-index', 100 - index);
+  });
 
   // Ensure textfields in a group properly overlap vertically.
   $('.text-group input').each(function(index) {
@@ -34,3 +54,27 @@ $(document).foundation();
   });
 
 }(jQuery));
+
+
+/*
+// Angular JS Application
+(function() {
+  angular.module('weddingApp', ['ngCurtain']);
+})();
+
+/*
+//var weddingApp = angular.module('weddingApp', ['ngCurtain']);
+
+nameApp.controller('NameCtrl', function ($scope){
+  $scope.firstName = 'John';
+
+  $scope.$watch('lastName', function(newValue, oldValue){
+    console.log('new value is ' + newValue);
+  });
+
+  setTimeout(function(){
+    $scope.lastName = 'Smith';
+    $scope.$apply();
+  }, 1000);
+});
+*/
