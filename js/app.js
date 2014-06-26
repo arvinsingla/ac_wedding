2645bdd614d33a6e8015951630abbeb8e891ae64
Arvin Singla <arvin.singla@gmail.com>
2014-06-26 03:15:02 -0400

    Fixed mobile version and scroll locking.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,41 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
+var docElem = window.document.documentElement, didScroll, scrollPosition;
+
+// trick to prevent scrolling when opening/closing button
+function noScrollFn() {
+  window.scrollTo( scrollPosition ? scrollPosition.x : 0, scrollPosition ? scrollPosition.y : 0 );
+}
+
+function noScroll() {
+  window.removeEventListener( 'scroll', scrollHandler );
+  window.addEventListener( 'scroll', noScrollFn );
+}
+
+function scrollFn() {
+  window.addEventListener( 'scroll', scrollHandler );
+}
+
+function canScroll() {
+  window.removeEventListener( 'scroll', noScrollFn );
+  scrollFn();
+}
+
+function scrollHandler() {
+  if( !didScroll ) {
+    didScroll = true;
+    setTimeout( function() { scrollPage(); }, 60 );
+  }
+};
+
+function scrollPage() {
+  scrollPosition = { x : window.pageXOffset || docElem.scrollLeft, y : window.pageYOffset || docElem.scrollTop };
+  didScroll = false;
+};
+
+scrollFn();
+
 // jQuery specific functionality for the front end.
 (function ($) {
 
@@ -18,6 +53,7 @@ $(document).foundation();
     $(this).toggleClass('open');
     $('body').toggleClass('locked');
     $modalContent.toggleClass('open');
+    noScroll();
   });
 
   // Modal close (X)
@@ -28,6 +64,7 @@ $(document).foundation();
     $button.toggleClass('open');
     $modalContent.toggleClass('open');
     $('body').toggleClass('locked');
+    canScroll();
   });
 
   // Function to lock the RSVP form and display thank you message.
